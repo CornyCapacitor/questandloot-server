40048e1b398b6885f8c38baeb959d1e523097310
Mateusz Minder <mateusz.minder99@gmail.com>
2024-11-09 15:33:14 +0100

    server.ts setup

--- server.ts
+++ server.ts
@@ -1,16 +1,40 @@
 import dotenv from 'dotenv'
 import express, { Application, Request, Response } from 'express'
+import http from 'http'
+import { WebSocket, WebSocketServer } from 'ws'
 
 // Env files config
 dotenv.config()
 
+// App setup
 const app: Application = express()
-const PORT = process.env.PORT || 3333
+const REST_PORT = process.env.REST_PORT || 3333
+const WS_PORT = process.env.WS_PORT || 3334
 
+// HTTP server & websocket server
+const server = http.createServer(app)
+const socket = new WebSocketServer({ server })
+
+// WEBSOCKET
+socket.on('connection', (ws: WebSocket) => {
+  console.log('Client connected')
+
+  // Disconnect event
+  socket.on('disconnect', () => {
+    console.log('Client disconnected')
+  })
+})
+
+// REST
 app.get('/', (req: Request, res: Response) => {
   res.send('Welcome to Express & TypeScript Server!')
 })
 
-app.listen(PORT, () => {
-  console.log(`Server is fire at https://localhost:${PORT}`)
+// Listeners
+app.listen(REST_PORT, () => {
+  console.log(`REST server is fire at https://localhost:${REST_PORT}`)
+})
+
+server.listen(WS_PORT, () => {
+  console.log(`WEBSOCKET server is fire at https://localhost:${WS_PORT}`)
 })
