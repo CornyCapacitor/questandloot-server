736547d86d7603a176d9a3350655b91f4f1268f2
Mateusz Minder <mateusz.minder99@gmail.com>
2025-02-16 20:00:17 +0100

    getCharacters endpoint polishment

--- controllers/characterControllers.ts
+++ controllers/characterControllers.ts
@@ -2,15 +2,34 @@ import { Request, Response } from 'express'
 import Character from '../models/characterModel'
 
 export const getCharacters = async (req: Request, res: Response): Promise<Response> => {
+  if (!req.user) {
+    return res.status(401).send({ error: 'User not authenticated' })
+  }
+
+  const page = parseInt(req.query.page as string) || 1
+  const limit = 50
+  const skip = (page - 1) * limit
+
   try {
-    const characters = await Character.find().select('-__v')
+    const characters = await Character.find()
+      .select('name title profession level -_id')
+      .skip(skip)
+      .limit(limit)
 
     if (!characters.length) {
       return res.status(404).send({ error: 'No characters found' })
     }
 
-    return res.status(200).send(characters)
+    const totalCharacters = await Character.countDocuments()
+    const totalPages = Math.ceil(totalCharacters / limit)
 
+    return res.status(200).send({
+      data: characters,
+      currentPage: page,
+      totalPages,
+      totalCharacters,
+      perPage: limit
+    })
   } catch (err) {
     if (err instanceof Error) {
       return res.status(500).send({ error: err.message })
--- routes/characters.ts
+++ routes/characters.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express'
 import { getCharacters } from '../controllers/characterControllers'
+import { authenticateTokenREST } from '../middlewares/authenticateTokenREST'
 import { asyncHandler } from '../utils/asyncHandler'
 
 const router = Router()
 
 // Login user
-router.get('/', asyncHandler(getCharacters))
+router.get('/', authenticateTokenREST, asyncHandler(getCharacters))
 
 export default router
